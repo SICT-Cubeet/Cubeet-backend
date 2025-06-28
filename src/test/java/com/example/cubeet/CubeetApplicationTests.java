56de879005c9b2f0c0ebee92a65e097376dc2ffa
jyun27 <kimjyun27@gmail.com>
2025-06-28 22:18:04 +0900

    add dockerfile

--- src/test/java/com/example/cubeet/CubeetApplicationTests.java
+++ src/test/java/com/example/cubeet/CubeetApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.cubeet;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CubeetApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.cubeet;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CubeetApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
