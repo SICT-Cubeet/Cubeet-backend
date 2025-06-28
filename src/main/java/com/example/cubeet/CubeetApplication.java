c9d90c8c7ffda630bbfb2773d11cb58cd232171c
jyun27 <kimjyun27@gmail.com>
2025-06-28 20:13:10 +0900

    first commit

---
+++ src/main/java/com/example/cubeet/CubeetApplication.java
@@ -0,0 +1,13 @@
+package com.example.cubeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubeetApplication.class, args);
+	}
+
+}
